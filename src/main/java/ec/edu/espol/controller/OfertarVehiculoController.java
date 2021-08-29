1cd45fceef16d2656c3e8424c0dd35d798a79b15
isaacsegovia27 <74276947+isaacsegovia27@users.noreply.github.com>
2021-08-29 16:48:44 -0500

    datos mostrados en tableview

--- src/main/java/ec/edu/espol/controller/OfertarVehiculoController.java
+++ src/main/java/ec/edu/espol/controller/OfertarVehiculoController.java
@@ -5,14 +5,18 @@
  */
 package ec.edu.espol.controller;
 
-import ec.edu.espol.model.Usuario;
 import ec.edu.espol.model.Vehiculo;
+import ec.edu.espol.model.Usuario;
 import ec.edu.espol.proyecto2p.App;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,8 +24,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
 /**
@@ -33,7 +39,7 @@ public class OfertarVehiculoController implements Initializable {
 
     private String correo;
     private String contraseña;
-    
+    private String tipo;
     private ArrayList<Usuario> usuarios;
     private ArrayList<Vehiculo> vehiculos;
     
@@ -41,7 +47,7 @@ public class OfertarVehiculoController implements Initializable {
     @FXML
     private Button atrasbtn;
     @FXML
-    private TableView tablaVehiculos;
+    private TableView<Vehiculo> tablaVehiculos;
     @FXML
     private ComboBox cbxtipo;
     @FXML
@@ -58,12 +64,32 @@ public class OfertarVehiculoController implements Initializable {
     private TextField maxPrecio;
     @FXML
     private Button buscarbtn;
+    @FXML
+    private TableColumn<Vehiculo,String> columnPlaca;
+    @FXML
+    private TableColumn<Vehiculo,String> columnMarca;
+    @FXML
+    private TableColumn<Vehiculo,String> columnModelo;
+    @FXML
+    private TableColumn<Vehiculo,String> columnMotor;
+    @FXML
+    private TableColumn<Vehiculo,String> columnColor;
+    @FXML
+    private TableColumn<Vehiculo,Integer> columnAnio;
+    @FXML
+    private TableColumn<Vehiculo,Double> columnRecorrido;
+    @FXML
+    private TableColumn<Vehiculo,Double> columnPrecio;
 
     /**
      * Initializes the controller class.
      */
+     
+     
+   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
         
         if (Usuario.recuperarUsuarios("usuarios.ser") == null) {
             this.usuarios = new ArrayList<Usuario>();
@@ -81,6 +107,20 @@ public class OfertarVehiculoController implements Initializable {
         tipos.add("Moto");
         tipos.add("Carro");
         tipos.add("Camioneta");
+        
+        columnPlaca.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getPlaca()));
+        columnMarca.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getMarca()));
+        columnModelo.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getModelo()));
+        columnMotor.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getMotor()));
+        columnColor.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getColor()));
+        /*
+        columnAnio.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getAnio()));
+        columnRecorrido.setCellValueFactory(data -> new SimpleDoubleProperty(data.getValue().getRecorrido()));
+        columnPrecio.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getPlaca()));
+          */    
+        
+       
+        
         
         cbxtipo.setItems(FXCollections.observableArrayList(tipos));
     }    
@@ -112,8 +152,23 @@ public class OfertarVehiculoController implements Initializable {
 
     @FXML
     private void buscarVehiculos(MouseEvent event) {
-    }
-    
+        ArrayList<Vehiculo> lista_vehiculo_fitrado = new ArrayList<>();
+        ArrayList<Vehiculo>lista_precio= Vehiculo.searchByPrecio(Double.parseDouble(minPrecio.getText()),Double.parseDouble(maxPrecio.getText()), vehiculos);
+         ArrayList<Vehiculo>lista_anio= Vehiculo.searchByAnio(Integer.parseInt(minAnio.getText()),Integer.parseInt(maxAnio.getText()), vehiculos);
+         ArrayList<Vehiculo>lista_recorrido= Vehiculo.searchByRecorrido(Double.parseDouble(minRecorrido.getText()),Double.parseDouble(maxRecorrido.getText()), vehiculos);
+        for(Vehiculo v : vehiculos){
+        if(lista_precio.contains(v) && lista_anio.contains(v) && lista_recorrido.contains(v) ){
+             lista_vehiculo_fitrado.add(v);
+             tablaVehiculos.setItems(FXCollections.observableArrayList( lista_vehiculo_fitrado)); 
+            
+        }  
+        }
+        
+        
+        
+        
+                }
+                
     public void setCorreo(String correo, String contraseña) {
         this.correo = correo;
         this.contraseña = contraseña;
